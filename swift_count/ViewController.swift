f497f19f033cd4dfd293b7e1ab30db2e02f23572
Minami Aramaki <minamidog373@gmail.com>
2014-11-15 15:40:49 +0900

    Initial Commit

---
+++ swift_count/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift_count
+//
+//  Created by Minami Sophia Aramaki on 2014/11/15.
+//  Copyright (c) 2014年 Minami Sophia Aramaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
